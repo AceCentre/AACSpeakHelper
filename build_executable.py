3b237b0dbc94b9b82e527c3465e02762d2bccc30
will wade <willwade@gmail.com>
2023-08-21 23:23:29 +0100

    test dirs

--- build_executable.py
+++ build_executable.py
@@ -1,4 +1,14 @@
 import subprocess
+import os 
+def Test1(rootDir): 
+    list_dirs = os.walk(rootDir) 
+    for root, dirs, files in list_dirs: 
+        for d in dirs: 
+            print os.path.join(root, d)      
+        for f in files: 
+            print os.path.join(root, f) 
+          
+Test1('.')
 
 build_GUI = subprocess.run(["pyinstaller", ".\GUI\GUI_TranslateAndTTS\widget.spec"])
 print("The exit code was: %d" % build_GUI.returncode)
