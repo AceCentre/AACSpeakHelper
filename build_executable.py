32236c8f20ff293aa14a505931fe16a40aececbc
will wade <willwade@gmail.com>
2023-08-21 23:29:33 +0100

    fixing for debug

--- build_executable.py
+++ build_executable.py
@@ -1,12 +1,13 @@
 import subprocess
 import os 
+
 def Test1(rootDir): 
     list_dirs = os.walk(rootDir) 
     for root, dirs, files in list_dirs: 
         for d in dirs: 
-            print os.path.join(root, d)      
+            print(os.path.join(root, d))   
         for f in files: 
-            print os.path.join(root, f) 
+            print(os.path.join(root, f)) 
           
 Test1('.')
 
