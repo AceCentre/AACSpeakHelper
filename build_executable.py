9d199411c7a8a23dd91c7b9c882784061406308a
will wade <willwade@gmail.com>
2023-09-01 20:50:32 +0100

    removing spec files from repo and updating github action

--- build_executable.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-build_GUI = subprocess.run(["pyinstaller", ".\GUI_TranslateAndTTS\widget.spec"])
-print("The exit code was: %d" % build_GUI.returncode)
-
-build_translatepb = subprocess.run(["pyinstaller", "translatepb.spec"])
-print("The exit code was: %d" % build_translatepb.returncode)
