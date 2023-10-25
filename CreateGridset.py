7f0a96b37846744895c2c35a563cd506ebf67b5b
will wade <willwade@gmail.com>
2023-10-25 12:49:34 +0100

    change to file path

--- CreateGridset.py
+++ CreateGridset.py
@@ -18,10 +18,11 @@ def modify_gridset(gridset_path, LocalAppPath):
 				with open(xml_path, 'r') as f:
 					filedata = f.read()
 				
-				full_path_to_exe = os.path.join(LocalAppPath, 'TranslateAndTTS', 'translatepb.exe')
+				local_app_data_path = os.environ.get('LOCALAPPDATA', '')
+				full_path_to_exe = os.path.join(local_app_data_path, 'Programs', 'Ace Centre', 'TranslateAndTTS', 'translatepb.exe')
 				full_path_to_exe_escaped = full_path_to_exe.replace('\\', '\\\\')
 				new_data = re.sub('%FILEPATHTOREPLACE%', full_path_to_exe_escaped, filedata)
-				
+							
 				with open(xml_path, 'w') as f:
 					f.write(new_data)
 		
