710c6a951bf3eea628d8fa23a2e963c4007da115
acerjay <acerjaycastillo@gmail.com>
2024-07-20 23:21:37 +0800

    Optimize imports and allow multiple request with different config file

--- translatepb.py
+++ translatepb.py
@@ -1,21 +1,25 @@
-import os
-import sys
-import time
 import asyncio
-import utils
+import json
 import logging
+import os
+import time
+
 import pyperclip
 import pyttsx3
-from tts_utils import speak
-from GUI_TranslateAndTTS.language_dictionary import *
+import pywintypes
+import win32file
+import win32pipe
 from PySide6.QtWidgets import *
 from deep_translator import *
-from utils import configure_app, config, args, clear_history
-import win32pipe, win32file, pywintypes
+import utils
+from GUI_TranslateAndTTS.language_dictionary import *
+import tts_utils
+# from utils import init, configure_app, config, args, clear_history
 
 
 def translate_clipboard():
     try:
+        config = utils.config
         translator = config.get('translate', 'provider')
         key = config.get('translate', f'{translator}_secret_key') if not translator == "GoogleTranslator" else None
         email = config.get('translate', 'email') if translator == 'MyMemoryTranslator' else None
@@ -88,6 +92,7 @@ def translate_clipboard():
 
 
 async def mainrun(listvoices: bool):
+    config = utils.config
     if listvoices:
         try:
             # Code that may raise an exception
@@ -100,7 +105,7 @@ async def mainrun(listvoices: bool):
                          'Configuration Setup?',
                 'List Voice Error')
             if result:
-                configure_app()
+                utils.configure_app()
             else:
                 return
         else:
@@ -126,7 +131,7 @@ async def mainrun(listvoices: bool):
             if not config.getboolean('TTS', 'bypass_tts', fallback=False):
                 start = time.perf_counter()
                 print(f"Text: [{clipboard}].")
-                speak(clipboard)
+                tts_utils.speak(clipboard)
                 stop = time.perf_counter() - start
                 # print(f"App runtime is {stop:0.5f} seconds.")
                 logging.info(f"App runtime is {stop:0.5f} seconds.")
@@ -135,17 +140,18 @@ async def mainrun(listvoices: bool):
             logging.info("------------------------------------------------------------------------")
         except Exception as e:
             logging.error("Runtime Error: {}".format(e), exc_info=True)
-            if args['preview']:
+            if utils.args['preview']:
                 return
             else:
                 result = utils.ynbox(str(e) + '\n\n Do You want to open the Configuration Setup?', 'Runtime Error')
                 if result:
-                    configure_app()
+                    utils.configure_app()
                 else:
                     return
 
 
 async def remove_stale_temp_files(directory_path, ignore_pattern=".db"):
+    config = utils.config
     start = time.perf_counter()
     current_time = time.time()
     day = int(config.get('appCache', 'threshold'))
@@ -154,7 +160,7 @@ async def remove_stale_temp_files(directory_path, ignore_pattern=".db"):
     for root, dirs, files in os.walk(directory_path):
         for file in files:
             file_path = os.path.join(root, file)
-            if ignore_pattern and file.endswith(ignore_pattern):
+            if ignore_pattern and file.endswith(ignore_pattern) and file.endswith('.db-journal'):
                 continue  # Ignore this file and move to the next iteration
             file_modification_time = os.path.getmtime(file_path)
             if file_modification_time < time_threshold:
@@ -167,14 +173,14 @@ async def remove_stale_temp_files(directory_path, ignore_pattern=".db"):
                     print(f"Error removing file {file_path}: {e}")
                     logging.error(f"Removed cache file: {file_path}", exc_info=True)
     stop = time.perf_counter() - start
-    clear_history(file_list)
+    utils.clear_history(file_list)
     # print(f"Cache clearing runtime is {stop:0.5f} seconds.")
     logging.info(f"Cache clearing is {stop:0.5f} seconds.")
     logging.info("------------------------------------------------------------------------")
 
 
 async def main(wav_files_path):
-    await asyncio.gather(mainrun(args['listvoices']), remove_stale_temp_files(wav_files_path))
+    await asyncio.gather(mainrun(utils.args['listvoices']), remove_stale_temp_files(wav_files_path))
 
 
 def pipe_server():
@@ -190,10 +196,12 @@ def pipe_server():
                 0,
                 None)
             logging.info("Pipe Server: Waiting for client...")
+            print(f'Waiting for request ...')
             win32pipe.ConnectNamedPipe(pipe, None)
             logging.info("Pipe Server: Client connected.")
         except pywintypes.error as e:
             logging.error(f"Failed to create or connect named pipe: {e}")
+            print(f"Failed to create or connect named pipe: Close all python.exe to restart the Pipe")
             continue  # Attempt to create a new pipe and wait for a new connection
 
         try:
@@ -201,11 +209,10 @@ def pipe_server():
                 # Read the sentence from the client
                 result, data = win32file.ReadFile(pipe, 64 * 1024)
                 if result == 0:  # Check if the read was successful
-                    # sentence = data.decode()
-                    from utils import configure_app, config, args, clear_history
+                    arguments = json.loads(data.decode())
+                    utils.init(args=arguments)
+                    tts_utils.init(utils)
                     asyncio.run(main(utils.audio_files_path))
-                    # print(sentence)
-                    win32file.WriteFile(pipe, data)
                 else:
                     break  # Break out of the inner loop if reading fails
         except pywintypes.error as e:
