7251851cb1a1b91975e12cd293af66a81d238bbe
will wade <willwade@gmail.com>
2024-08-28 21:27:00 +0100

    try this approach to get site packages dir for poetry

--- build_executable.bat
+++ build_executable.bat
@@ -1,12 +1,13 @@
 @echo off
 
-rem Capture the site-packages path
-for /f "delims=" %%i in ('poetry run python -c "import site; print(site.getsitepackages()[0])"') do set site_packages=%%i
+rem Capture the Poetry environment path
+for /f "delims=" %%i in ('poetry env info --path') do set venv_path=%%i
 
-rem Echo the captured path for debugging
+rem Construct the site-packages path
+set site_packages=%venv_path%\Lib\site-packages
+
+rem Echo the constructed site-packages path for debugging
 echo Site packages path: %site_packages%
-
-rem Run PyInstaller with the correct site-packages path
 
 poetry run python -m PyInstaller AACSpeakHelperServer.py --noupx --onedir --noconsole --name "AACSpeakHelperServer" -i .\assets\translate.ico --clean --add-binary "%site_packages%/azure/cognitiveservices/speech/Microsoft.CognitiveServices.Speech.core.dll;." --collect-all language_data --collect-all language_tags --collect-all comtypes --collect-all pytz -y
 poetry run python -m PyInstaller .\GUI_TranslateAndTTS\widget.py --noupx --noconsole --name "Configure AACSpeakHelper" --onedir -i .\assets\configure.ico --clean --add-binary "%site_packages%/azure/cognitiveservices/speech/Microsoft.CognitiveServices.Speech.core.dll;." --collect-all language_data --collect-all language_tags --collect-all pytz --collect-all comtypes -y
