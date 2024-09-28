92fd58b0a3cc8462ae610a8d5cbc7d3797a71e89
will wade <willwade@gmail.com>
2024-09-28 06:58:27 +0100

    alter sys,path append

--- GUI_TranslateAndTTS/widget.py
+++ GUI_TranslateAndTTS/widget.py
@@ -9,7 +9,9 @@ import sys
 import time
 import uuid
 import warnings
-sys.path.append(os.path.abspath('..'))
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_root)
 from configure_enc_utils import load_config, load_credentials
 
 warnings.filterwarnings("ignore")
