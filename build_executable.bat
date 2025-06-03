e9bff99b8c8fbd801ee5f5d922f7494014b12375
will wade <willwade@gmail.com>
2025-06-03 22:43:59 +0100

    fix aiohttp errors!

--- build_executable.bat
+++ build_executable.bat
@@ -11,11 +11,11 @@ uv run python -m PySide6.uic.pyuic GUI_TranslateAndTTS/form.ui -o GUI_TranslateA
 uv run python -m PySide6.uic.pyuic GUI_TranslateAndTTS/item.ui -o GUI_TranslateAndTTS/item.py
 
 REM Build Python executables with PyInstaller
-uv run python -m PyInstaller AACSpeakHelperServer.py --noupx --onedir --noconsole --name "AACSpeakHelperServer" -i .\assets\translate.ico --clean --add-binary "%site_packages%\azure\cognitiveservices\speech\Microsoft.CognitiveServices.Speech.core.dll;." --add-binary "C:\Windows\System32\winmm.dll;." --add-binary "C:\Windows\System32\dsound.dll;." --collect-all language_data --collect-all language_tags --collect-all comtypes --collect-all pytz --collect-all sounddevice --collect-all pyaudio -y
+uv run python -m PyInstaller AACSpeakHelperServer.py --noupx --onedir --noconsole --name "AACSpeakHelperServer" -i .\assets\translate.ico --clean --collect-binaries azure.cognitiveservices.speech --add-binary "C:\Windows\System32\winmm.dll;." --add-binary "C:\Windows\System32\dsound.dll;." --add-binary "C:\Windows\System32\AudioEng.dll;." --add-binary "C:\Windows\System32\AudioSes.dll;." --add-binary "C:\Windows\System32\AUDIOKSE.dll;." --add-binary "C:\Windows\System32\mf.dll;." --add-binary "C:\Windows\System32\mfplat.dll;." --add-binary "C:\Windows\System32\MMDevAPI.dll;." --collect-all language_data --collect-all language_tags --collect-all comtypes --collect-all pytz --collect-all sounddevice --collect-all pyaudio -y
 
-uv run python -m PyInstaller .\GUI_TranslateAndTTS\widget.py --noupx --noconsole --name "Configure AACSpeakHelper" --onedir -i .\assets\configure.ico --clean --add-binary "%site_packages%\azure\cognitiveservices\speech\Microsoft.CognitiveServices.Speech.core.dll;." --collect-all language_data --collect-all language_tags --collect-all pytz --collect-all comtypes -y
+uv run python -m PyInstaller .\GUI_TranslateAndTTS\widget.py --noupx --noconsole --name "Configure AACSpeakHelper" --onedir -i .\assets\configure.ico --clean --collect-binaries azure.cognitiveservices.speech --collect-all language_data --collect-all language_tags --collect-all pytz --collect-all comtypes -y
 
-uv run python -m PyInstaller client.py --noupx --noconsole --onedir --clean -i .\assets\translate.ico -y
+uv run python -m PyInstaller client.py --noupx --console --onedir --clean -i .\assets\translate.ico -y
 
 uv run python -m PyInstaller cli_config_creator.py --noupx --console --name "Configure AACSpeakHelper CLI" --onedir --clean -i .\assets\configure.ico -y
 
